0e20bda63bc7a9b1772a88bb3821fb97b47cfb8d
nonogon <leslie.strauss@gmail.com>
2014-10-17 15:32:13 -0700

    Progress on create new post functionality.

--- scripts/app.js
+++ scripts/app.js
@@ -19,29 +19,44 @@ App.Post1Route = Ember.Route.extend({
 });
 
 App.NewPostController = Ember.ObjectController.extend({
+  post: function(key, value) {
+    if (arguments.length >1) {
+      var post = value.split(/\s+/);
+      this.set('title', postContents[0]);
+      this.set('body', postContents[1]);
+    }
+
   actions: {
-    createUser: function(user) {
-      ........
-    },
-    cancelUserCreation: function() {
-      .......
+    createPost: function(post) {
+    return this.get('title') + "\n" + this.get('body')
+    }.property('title', 'body')
+
+    cancelCreatePost: function() {
+      alert("Canceled New Post.");
     }
   }
-
 });
 
-App.UserFormComponent = Ember.Component.extend 
+App.NewPostComponent = Ember.Component.extend({
+  actions: {
+    submit: function() {
+      this.sendAction('submit', {
+        title: this.get('title'),
+        body: this.get('body')
+      });
+    }
+    cancel: function() {
+      this.sendAction('cancel');
+    }
+  }
+});
 
-
-//   actions: {
-//     submitPost: function() {
-//       this.sendAction{'submit',
-//       {
-//         title: this.get('title'),
-//         body: this.get('body')
-//       });
-//     },
-
-//   }
-// });
+App.NewPostRoute = Ember.Route.extend({
+  model: function(post) {
+    return {
+      title: this.get('title'),
+      body: this.get('body')
+    };
+  }
+});
 
