d7de6c77c91f29488b8f05535d093ea1a26996cb
nonogon <leslie.strauss@gmail.com>
2014-10-17 16:24:20 -0700

    Prior to whit doing things.

--- scripts/app.js
+++ scripts/app.js
@@ -7,37 +7,30 @@ console.log('welcome to leslie world');
 App.Router.map(function() {
   this.route('newPost');
   this.route('post1');
+  this.route('create-post', { path: "/components"});
 });
 
 App.Post1Route = Ember.Route.extend({
   model: function() {
     return {
       title: "First Entry",
-      body: "Today, I finally fell in love with my wife."
+      body: "It rained today."
     };
   }
 });
 
-App.NewPostController = Ember.ObjectController.extend({
-  post: function(key, value) {
-    if (arguments.length >1) {
-      var post = value.split(/\s+/);
-      this.set('title', postContents[0]);
-      this.set('body', postContents[1]);
-    }
-
+App.CreatePostController = Ember.ObjectController.extend({
   actions: {
-    createPost: function(post) {
-    return this.get('title') + "\n" + this.get('body')
-    }.property('title', 'body')
-
+    createPost: function(create) {
+      alert("New Post Created.");
+    },
     cancelCreatePost: function() {
       alert("Canceled New Post.");
     }
   }
 });
 
-App.NewPostComponent = Ember.Component.extend({
+App.CreatePostComponent = Ember.Component.extend({
   actions: {
     submit: function() {
       this.sendAction('submit', {
@@ -45,18 +38,5 @@ App.NewPostComponent = Ember.Component.extend({
         body: this.get('body')
       });
     }
-    cancel: function() {
-      this.sendAction('cancel');
-    }
   }
 });
-
-App.NewPostRoute = Ember.Route.extend({
-  model: function(post) {
-    return {
-      title: this.get('title'),
-      body: this.get('body')
-    };
-  }
-});
-
