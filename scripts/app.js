cb05e474a0a68d7f01b32dc6de551e8311d31b9b
nonogon <leslie.strauss@gmail.com>
2014-10-17 16:37:00 -0700

    Removed  components, cleaned up code.

--- scripts/app.js
+++ scripts/app.js
@@ -4,39 +4,42 @@ var App = Ember.Application.create({
 
 console.log('welcome to leslie world');
 
+var posts = [{
+  title: "First Entry",
+  body: "It rained today."
+}, {
+  title: "Second Entry",
+  body: "It rained yesterday."
+}];
+
 App.Router.map(function() {
-  this.route('newPost');
-  this.route('post1');
-  this.route('create-post', { path: "/components"});
+  this.route('post1'); // TODO: this needs to be generalized to handle any post
+  this.route('create-post');
 });
 
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return posts;
+  }
+});
+
+// TODO: this also needs to be generalized to handle any post
 App.Post1Route = Ember.Route.extend({
   model: function() {
-    return {
-      title: "First Entry",
-      body: "It rained today."
-    };
+    return posts[0];
   }
 });
 
 App.CreatePostController = Ember.ObjectController.extend({
   actions: {
     createPost: function(create) {
+      // this needs to update stuff properly. it will be somehow
+      // related to the concept of computed properties, but maybe
+      // not quite in the way that you think.
       alert("New Post Created.");
     },
     cancelCreatePost: function() {
       alert("Canceled New Post.");
-    }
-  }
-});
-
-App.CreatePostComponent = Ember.Component.extend({
-  actions: {
-    submit: function() {
-      this.sendAction('submit', {
-        title: this.get('title'),
-        body: this.get('body')
-      });
     }
   }
 });
